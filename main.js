0917f8c28df56005b814646e6d74d18f75671c7e
mgobat <45852490+mgobat@users.noreply.github.com>
2020-02-19 09:01:38 -0600

    Add standard "Edit" menu for macOS which enables copy/paste in "Configuration" dialog.

--- main.js
+++ main.js
@@ -296,6 +296,45 @@ const mainMenuTemplate = [
           app.quit();
         }
       }
+    ],
+  },
+  {
+    label: 'Edit',
+    visible: false,
+    submenu: [
+      {
+        label: "Undo",
+        accelerator: "CmdOrCtrl+Z",
+        selector: "undo:"
+      },
+      {
+        label: "Redo",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        selector: "redo:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:"
+       },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:"
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:"
+      }
     ]
   }
 ]
@@ -346,6 +385,8 @@ function setMenus(){
         {role: 'quit'}
       ]
     })
+    //Make the "Edit" menu visible, only for macOS.
+    mainMenuTemplate[menuOffset + 1].visible = true;
   }
 
   if (configSettings.interval == 0) {
