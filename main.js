4cb94273692b7351959ccf1b49d2d302985de9c4
mgobat <45852490+mgobat@users.noreply.github.com>
2023-08-29 12:48:04 -0500

    Remove beta version number for 2.2 general release

--- main.js
+++ main.js
@@ -290,7 +290,7 @@ function createWindow () {
       width: 600,
       height: 625,
       show: true,
-      title: "Alma Print Daemon 2.2.0-beta-03",
+      title: "Alma Print Daemon 2.2.0",
       webPreferences: {
         //preload: path.join(__dirname, 'preload.js'),
         nodeIntegration: true
